bb9102c0ec880eb96cd8666caacf831fdea9f548
Nathan Baggs <hello@nathanbaggs.com>
2025-11-26 21:40:13 +0000

    Integrate changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+#include <variant>
+
 #include <windows.h>
 
 #include "config.h"
+#include "events/key_event.h"
+#include "graphics/opengl.h"
+#include "graphics/window.h"
 #include "utils/formatter.h"
 #include "utils/hello.h"
 #include "utils/log.h"
@@ -8,10 +13,40 @@
 
 int main()
 {
+    // Daz_Da_Cat: First stream done.
     ::CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 
     ufps::log::info("μfps version: {}.{}.{}", ufps::version::major, ufps::version::minor, ufps::version::patch);
     ufps::log::info("{}", ufps::system_info());
+
+    auto window = ufps::Window{ufps::WindowMode::WINDOWED, 1920u, 1080u, 1920u, 0u};
+    auto running = true;
+
+    while (running)
+    {
+        ::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        auto event = window.pump_event();
+        while (event && running)
+        {
+            std::visit(
+                [&](auto &&arg)
+                {
+                    using T = std::decay_t<decltype(arg)>;
+
+                    if constexpr (std::same_as<T, ufps::KeyEvent>)
+                    {
+                        ufps::log::info("stopping");
+                        running = false;
+                    }
+                },
+                *event);
+
+            event = window.pump_event();
+        }
+
+        window.swap();
+    }
 
     return 0;
 }
