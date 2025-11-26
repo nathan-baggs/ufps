f77d226e433d46863d69f0bcda0ef022daecb1d9
Nathan Baggs <hello@nathanbaggs.com>
2025-11-26 22:00:13 +0000

    Integrate changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <string_view>
 #include <variant>
 
 #include <windows.h>
@@ -5,11 +6,25 @@
 #include "config.h"
 #include "events/key_event.h"
 #include "graphics/opengl.h"
+#include "graphics/shader.h"
 #include "graphics/window.h"
 #include "utils/formatter.h"
 #include "utils/hello.h"
 #include "utils/log.h"
 #include "utils/system_info.h"
+
+using namespace std::literals;
+
+namespace
+{
+constexpr auto sample_shader = R"(
+#version 460 core
+void main()
+{
+    gl_Position = vec4(1.0f);
+}
+)"sv;
+}
 
 int main()
 {
@@ -21,6 +36,8 @@ int main()
 
     auto window = ufps::Window{ufps::WindowMode::WINDOWED, 1920u, 1080u, 1920u, 0u};
     auto running = true;
+
+    [[maybe_unused]] auto simple_vert = ufps::Shader{sample_shader, ufps::ShaderType::VERTEX, "sample_shader"sv};
 
     while (running)
     {
