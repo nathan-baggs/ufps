6bc8da7f1d9052d2a894a52b23d7477a04b6627e
Nathan Baggs <hello@nathanbaggs.com>
2025-11-30 21:24:55 +0000

    Add Buffer class

---
+++ src/graphics/buffer.cpp
@@ -0,0 +1,33 @@
+#include "graphics/buffer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#include "graphics/opengl.h"
+#include "utils/auto_release.h"
+#include "utils/data_buffer.h"
+#include "utils/error.h"
+
+namespace ufps
+{
+
+Buffer::Buffer(std::uint32_t size)
+    : buffer_{0u, [](auto buffer) { ::glDeleteBuffers(1, &buffer); }}
+    , size_{size}
+{
+    ::glCreateBuffers(1, &buffer_);
+    ::glNamedBufferStorage(buffer_, size, nullptr, GL_DYNAMIC_STORAGE_BIT);
+}
+
+auto Buffer::write(DataBufferView data, std::size_t offset) const -> void
+{
+    expect(size_ >= data.size_bytes() + offset, "buffer too small");
+    ::glNamedBufferSubData(buffer_, offset, data.size(), data.data());
+}
+
+auto Buffer::native_handle() const -> ::GLuint
+{
+    return buffer_;
+}
+
+}
