4dc6a35ef5a1b45b7b8f599621fad95803178c83
Nathan Baggs <hello@nathanbaggs.com>
2025-11-30 21:56:54 +0000

    Flash vertex colour

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "config.h"
 #include "events/key_event.h"
 #include "graphics/buffer.h"
+#include "graphics/colour.h"
 #include "graphics/opengl.h"
 #include "graphics/program.h"
 #include "graphics/shader.h"
@@ -27,6 +28,7 @@ constexpr auto sample_vertex_shader = R"(
 struct VertexData
 {
     float position[3];
+    float colour[3];
 };
 
 layout(binding = 0, std430) readonly buffer vertices {
@@ -41,20 +43,33 @@ vec3 get_position(int index)
         data[index].position[2]);
 }
 
+vec3 get_colour(int index)
+{
+    return vec3(
+        data[index].colour[0],
+        data[index].colour[1],
+        data[index].colour[2]);
+}
+
+layout (location = 0) out vec3 out_colour;
+
 void main()
 {
     gl_Position = vec4(get_position(gl_VertexID), 1.0);
+    out_colour = get_colour(gl_VertexID);
 }
 )"sv;
 
 constexpr auto sample_fragment_shader = R"(
 #version 460 core
 
-layout(location = 0) out vec4 colour;
+layout(location = 0) in vec3 in_colour;
+
+layout(location = 0) out vec4 out_colour;
 
 void main()
 {
-    colour = vec4(0.0, 0.5, 1.0, 1.0);
+    out_colour = vec4(in_colour, 1.0);
 }
 )"sv;
 
@@ -84,10 +99,10 @@ int main()
         ufps::Shader{sample_fragment_shader, ufps::ShaderType::FRAGMENT, "sample_fragment_shader"sv};
     const auto sample_prog = ufps::Program{sample_vert, sample_frag, "sample_prog"sv};
 
-    const ufps::VertexData triangle[] = {
-        {{0.0f, 0.5f, 0.0f}},
-        {{-0.5f, -0.5f, 0.0f}},
-        {{0.5f, -0.5f, 0.0f}},
+    ufps::VertexData triangle[] = {
+        {{0.0f, 0.5f, 0.0f}, ufps::colours::azure},
+        {{-0.5f, -0.5f, 0.0f}, ufps::Colour{0.6, 0.1, 0.0}},
+        {{0.5f, -0.5f, 0.0f}, ufps::Colour{0.42, 0.42, 0.42}},
     };
 
     const auto triangle_view = ufps::DataBufferView{reinterpret_cast<const std::byte *>(triangle), sizeof(triangle)};
@@ -135,6 +150,14 @@ int main()
 
             event = window.pump_event();
         }
+
+        triangle[0].colour.r += 0.01f;
+        if (triangle[0].colour.r >= 1.0f)
+        {
+            triangle[0].colour.r = 0.0f;
+        }
+
+        triangle_buffer.write(triangle_view, 0zu);
 
         ::glMultiDrawArraysIndirect(GL_TRIANGLES, nullptr, 1, 0);
 
