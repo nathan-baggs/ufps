33adb55a2669861be237b6c23aef522db547d7f9
Nathan Baggs <hello@nathanbaggs.com>
2025-12-03 21:29:54 +0000

    Add MultiBuffer class and tests

---
+++ src/graphics/multi_buffer.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <cstddef>
+#include <string_view>
+
+#include "src/utils/data_buffer.h"
+
+namespace ufps
+{
+
+template <class T>
+concept IsBuffer = requires(T t, DataBufferView data, std::size_t offset) {
+    { t.write(data, offset) };
+};
+
+/**
+ * A multi buffer wrapper over a Buffer type. Will allocate size * Frames amount of data and can advance through the
+ * internal frames
+ */
+template <IsBuffer Buffer, std::size_t Frames = 3zu>
+class MultiBuffer
+{
+  public:
+    MultiBuffer(std::size_t size, std::string_view name)
+        : buffer_{size * Frames, name}
+        , original_size_{size}
+        , frame_offset_{}
+    {
+    }
+
+    auto write(DataBufferView data, std::size_t offset) -> void
+    {
+        buffer_.write(data, offset + frame_offset_);
+    }
+
+    auto advance() -> void
+    {
+        frame_offset_ = (frame_offset_ + original_size_) % (original_size_ * Frames);
+    }
+
+    auto buffer() const -> const Buffer &
+    {
+        return buffer_;
+    }
+
+  private:
+    Buffer buffer_;
+    std::size_t original_size_;
+    std::size_t frame_offset_;
+};
+
+}
---
+++ tests/multi_buffer_tests.cpp
@@ -0,0 +1,123 @@
+#include <cstddef>
+#include <string_view>
+#include <tuple>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "graphics/multi_buffer.h"
+#include "utils/data_buffer.h"
+
+using namespace std::literals;
+
+struct FakeBuffer
+{
+    FakeBuffer(std::size_t size, std::string_view name)
+        : size{size}
+        , name{name}
+    {
+    }
+
+    auto write(ufps::DataBufferView data, std::size_t offset) -> void
+    {
+        write_calls.push_back({data.data(), offset});
+    }
+
+    std::vector<std::tuple<const std::byte *, std::size_t>> write_calls;
+    std::size_t size;
+    std::string_view name;
+};
+
+TEST(multi_frame, single_write)
+{
+    auto data = ufps::DataBuffer{std::byte{0x0}, std::byte{0x1}, std::byte{0x2}};
+    auto data_view = ufps::DataBufferView{data};
+
+    auto mb = ufps::MultiBuffer<FakeBuffer, 3zu>{data_view.size_bytes(), "test_buffer"};
+    mb.write(data_view, 0);
+
+    const auto &buffer = mb.buffer();
+
+    const auto expected = std::vector{std::make_tuple(data_view.data(), 0zu)};
+    ASSERT_EQ(buffer.size, data_view.size_bytes() * 3zu);
+    ASSERT_EQ(buffer.write_calls, expected);
+    ASSERT_EQ(buffer.name, "test_buffer"sv);
+}
+
+TEST(multi_frame, triple_write)
+{
+    auto data = ufps::DataBuffer{std::byte{0x0}, std::byte{0x1}, std::byte{0x2}};
+    auto data_view = ufps::DataBufferView{data};
+
+    auto mb = ufps::MultiBuffer<FakeBuffer, 3zu>{data_view.size_bytes(), "test_buffer"};
+    mb.write(data_view, 0);
+    mb.advance();
+    mb.write(data_view, 0);
+    mb.advance();
+    mb.write(data_view, 0);
+
+    const auto &buffer = mb.buffer();
+
+    const auto expected = std::vector{
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 0zu),
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 1zu),
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 2zu),
+    };
+
+    ASSERT_EQ(buffer.size, data_view.size_bytes() * 3zu);
+    ASSERT_EQ(buffer.write_calls, expected);
+}
+
+TEST(multi_frame, quad_write)
+{
+    auto data = ufps::DataBuffer{std::byte{0x0}, std::byte{0x1}, std::byte{0x2}};
+    auto data_view = ufps::DataBufferView{data};
+
+    auto mb = ufps::MultiBuffer<FakeBuffer, 3zu>{data_view.size_bytes(), "test_buffer"};
+    mb.write(data_view, 0);
+    mb.advance();
+    mb.write(data_view, 0);
+    mb.advance();
+    mb.write(data_view, 0);
+    mb.advance();
+    mb.write(data_view, 0);
+
+    const auto &buffer = mb.buffer();
+
+    const auto expected = std::vector{
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 0zu),
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 1zu),
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 2zu),
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 0zu),
+    };
+
+    ASSERT_EQ(buffer.size, data_view.size_bytes() * 3zu);
+    ASSERT_EQ(buffer.write_calls, expected);
+}
+
+TEST(multi_frame, multi_write_offset)
+{
+    auto data = ufps::DataBuffer{std::byte{0x0}, std::byte{0x1}, std::byte{0x2}, std::byte{0x3}, std::byte{0x4}};
+    auto data_view = ufps::DataBufferView{data};
+
+    auto mb = ufps::MultiBuffer<FakeBuffer, 3zu>{data_view.size_bytes(), "test_buffer"};
+    mb.write(data_view, 1);
+    mb.advance();
+    mb.write(data_view, 2);
+    mb.advance();
+    mb.write(data_view, 3);
+    mb.advance();
+    mb.write(data_view, 4);
+
+    const auto &buffer = mb.buffer();
+
+    const auto expected = std::vector{
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 0zu + 1zu),
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 1zu + 2zu),
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 2zu + 3zu),
+        std::make_tuple(data_view.data(), data_view.size_bytes() * 0zu + 4zu),
+    };
+
+    ASSERT_EQ(buffer.size, data_view.size_bytes() * 3zu);
+    ASSERT_EQ(buffer.write_calls, expected);
+}
