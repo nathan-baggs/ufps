6416560ed090593d38a7fe9d072833a777474e23
Nathan Baggs <hello@nathanbaggs.com>
2025-12-07 21:31:56 +0000

    Add CommandBuffer class

---
+++ src/graphics/command_buffer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include "graphics/multi_buffer.h"
+#include "graphics/opengl.h"
+#include "graphics/persistent_buffer.h"
+#include "graphics/scene.h"
+
+namespace ufps
+{
+
+class CommandBuffer
+{
+  public:
+    CommandBuffer();
+    auto build(const Scene &scene) -> std::uint32_t;
+    auto native_handle() const -> ::GLuint;
+    auto advance() -> void;
+    auto to_string() const -> std::string;
+
+  private:
+    MultiBuffer<PersistentBuffer> command_buffer_;
+};
+
+}
