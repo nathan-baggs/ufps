2cd05d4ec8ba44a16240cfa550119247ca72d149
Nathan Baggs <hello@nathanbaggs.com>
2025-12-07 21:46:42 +0000

    Refactor main to use Renderer

--- src/main.cpp
+++ src/main.cpp
@@ -6,77 +6,17 @@
 
 #include "config.h"
 #include "events/key_event.h"
-#include "graphics/buffer.h"
 #include "graphics/colour.h"
 #include "graphics/command_buffer.h"
-#include "graphics/entity.h"
 #include "graphics/mesh_manager.h"
-#include "graphics/opengl.h"
-#include "graphics/program.h"
+#include "graphics/renderer.h"
 #include "graphics/scene.h"
-#include "graphics/shader.h"
 #include "graphics/window.h"
-#include "utils/data_buffer.h"
 #include "utils/formatter.h"
 #include "utils/log.h"
 #include "utils/system_info.h"
 
 using namespace std::literals;
-
-namespace
-{
-constexpr auto sample_vertex_shader = R"(
-#version 460 core
-    
-struct VertexData
-{
-    float position[3];
-    float colour[3];
-};
-
-layout(binding = 0, std430) readonly buffer vertices {
-    VertexData data[];
-};
-
-vec3 get_position(int index)
-{
-    return vec3(
-        data[index].position[0],
-        data[index].position[1],
-        data[index].position[2]);
-}
-
-vec3 get_colour(int index)
-{
-    return vec3(
-        data[index].colour[0],
-        data[index].colour[1],
-        data[index].colour[2]);
-}
-
-layout (location = 0) out vec3 out_colour;
-
-void main()
-{
-    gl_Position = vec4(get_position(gl_VertexID), 1.0);
-    out_colour = get_colour(gl_VertexID);
-}
-)"sv;
-
-constexpr auto sample_fragment_shader = R"(
-#version 460 core
-
-layout(location = 0) in vec3 in_colour;
-
-layout(location = 0) out vec4 out_colour;
-
-void main()
-{
-    out_colour = vec4(in_colour, 1.0);
-}
-)"sv;
-
-}
 
 int main()
 {
@@ -90,13 +30,8 @@ int main()
     auto window = ufps::Window{ufps::WindowMode::WINDOWED, 1920u, 1080u, 1920u, 0u};
     auto running = true;
 
-    const auto sample_vert = ufps::Shader{sample_vertex_shader, ufps::ShaderType::VERTEX, "sample_vertex_shader"sv};
-    const auto sample_frag =
-        ufps::Shader{sample_fragment_shader, ufps::ShaderType::FRAGMENT, "sample_fragment_shader"sv};
-    const auto sample_prog = ufps::Program{sample_vert, sample_frag, "sample_prog"sv};
-
     auto mesh_manager = ufps::MeshManager{};
-    auto command_buffer = ufps::CommandBuffer{};
+    auto renderer = ufps::Renderer{};
 
     auto scene = ufps::Scene{.entities = {}, .mesh_manager = mesh_manager};
 
@@ -111,13 +46,6 @@ int main()
              {{{0.0f, 0.0f, 0.0f}, ufps::colours::azure},
               {{-0.5f, 0.5f, 0.0f}, ufps::Colour{0.42, 0.42, 0.42}},
               {{0.0f, 0.5f, 0.0f}, ufps::Colour{0.6, 0.1, 0.0}}})});
-
-    auto dummy_vao = ufps::AutoRelease<::GLuint>{0u, [](auto e) { ::glDeleteVertexArrays(1, &e); }};
-    ::glGenVertexArrays(1, &dummy_vao);
-
-    ::glBindVertexArray(dummy_vao);
-    ::glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, mesh_manager.native_handle());
-    sample_prog.use();
 
     while (running)
     {
@@ -142,13 +70,7 @@ int main()
             event = window.pump_event();
         }
 
-        const auto command_count = command_buffer.build(scene);
-
-        ::glBindBuffer(GL_DRAW_INDIRECT_BUFFER, command_buffer.native_handle());
-
-        ::glMultiDrawArraysIndirect(GL_TRIANGLES, nullptr, command_count, 0);
-
-        command_buffer.advance();
+        renderer.render(scene);
 
         window.swap();
     }
