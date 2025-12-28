024fef194fced04c289439b30b1357ac7b3a0361
Nathan Baggs <hello@nathanbaggs.com>
2025-12-28 21:32:06 +0000

    Add helper function to parse image formats

---
+++ src/graphics/utils.cpp
@@ -0,0 +1,55 @@
+#include "graphics/utils.h"
+
+#include <memory>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "graphics/texture_data.h"
+#include "utils/data_buffer.h"
+#include "utils/error.h"
+
+namespace
+{
+
+auto channels_to_format(int num_channels) -> ufps::TextureFormat
+{
+    switch (num_channels)
+    {
+        case 3: return ufps::TextureFormat::RGB;
+        case 4: return ufps::TextureFormat::RGBA;
+    }
+
+    throw ufps::Exception("unsupported channel count: {}", num_channels);
+}
+
+}
+
+namespace ufps
+{
+auto load_texture(DataBufferView image_data) -> TextureData
+{
+    auto width = int{};
+    auto height = int{};
+    auto num_channels = int{};
+
+    auto raw_data = std::unique_ptr<::stbi_uc, void (*)(void *)>{
+        ::stbi_load_from_memory(
+            reinterpret_cast<const ::stbi_uc *>(image_data.data()),
+            image_data.size(),
+            &width,
+            &height,
+            &num_channels,
+            0),
+        ::stbi_image_free};
+    ensure(raw_data, "failed to parse texture data");
+
+    const auto *ptr = reinterpret_cast<const std::byte *>(raw_data.get());
+
+    return {
+        .width = static_cast<std::uint32_t>(width),
+        .height = static_cast<std::uint32_t>(height),
+        .format = channels_to_format(num_channels),
+        .data = {ptr, ptr + width * height * num_channels}};
+}
+}
--- src/graphics/utils.h
+++ src/graphics/utils.h
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "graphics/opengl.h"
+#include "graphics/texture_data.h"
 #include "utils/data_buffer.h"
 #include "utils/log.h"
 
@@ -36,4 +37,7 @@ auto resize_gpu_buffer(const std::vector<T> &cpu_buffer, Buffer &gpu_buffer, std
         gpu_buffer = Buffer{new_size, name};
     }
 }
+
+auto load_texture(DataBufferView image_data) -> TextureData;
+
 }
