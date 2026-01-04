8211af3a9a5efb07d45287cf7a345341ed8afee6
Nathan Baggs <hello@nathanbaggs.com>
2026-01-04 21:12:43 +0000

    Add normal to VertexData

--- src/graphics/vertex_data.h
+++ src/graphics/vertex_data.h
@@ -14,9 +14,10 @@ struct UV
 struct VertexData
 {
     Vector3 position;
+    Vector3 normal;
     UV uv;
 };
 
-static_assert(sizeof(VertexData) == sizeof(float) * 3 + sizeof(float) * 2);
+static_assert(sizeof(VertexData) == sizeof(float) * 3 + sizeof(float) * 3 + sizeof(float) * 2);
 
 }
