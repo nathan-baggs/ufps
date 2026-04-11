8cdd56cb52bc4af7b9c4624d6eab49550c59a36e
Nathan Baggs <hello@nathanbaggs.com>
2026-04-11 10:58:55 +0100

    Integrate changes

--- src/graphics/debug_renderer.cpp
+++ src/graphics/debug_renderer.cpp
@@ -240,6 +240,7 @@ auto DebugRenderer::post_render(Scene &scene) -> void
     const auto cube_parts = scene.mesh_manager().mesh("cube");
     ensure(cube_parts.size() == 1u, "cube mesh should have exactly 1 part");
     const auto cube_indices_offset_bytes = cube_parts.front().index_offset * sizeof(std::uint32_t);
+    const auto cube_vertex_offset = cube_parts.front().vertex_offset;
 
     for (const auto &light : scene.lights().lights)
     {
@@ -254,7 +255,12 @@ auto DebugRenderer::post_render(Scene &scene) -> void
 
         debug_light_program_.set_uniforms(light_model, light.colour);
 
-        ::glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, reinterpret_cast<const void *>(cube_indices_offset_bytes));
+        ::glDrawElementsBaseVertex(
+            GL_TRIANGLES,
+            36,
+            GL_UNSIGNED_INT,
+            reinterpret_cast<const void *>(cube_indices_offset_bytes),
+            cube_vertex_offset);
     }
 
     debug_light_program_.unbind();
@@ -440,8 +446,10 @@ auto DebugRenderer::post_render(Scene &scene) -> void
         std::ranges::max(scaled_histogram),
         ::ImVec2(::ImGui::GetContentRegionAvail().x, 150.0f));
 
-    const auto mesh_names_cstr = mesh_manager_.mesh_names() |
-                                 std::views::transform([](const auto &e) { return e.c_str(); }) |
+    const auto mesh_names = scene.mesh_manager().mesh_names();
+    const auto mesh_names_cstr = mesh_names |                                                     //
+                                 std::views::filter([](const auto &e) { return !e.empty(); }) |   //
+                                 std::views::transform([](const auto &e) { return e.c_str(); }) | //
                                  std::ranges::to<std::vector>();
 
     auto mesh_selected_index = std::optional<std::uint32_t>{};
@@ -595,6 +603,44 @@ auto DebugRenderer::post_render(Scene &scene) -> void
             }
 
             ::ImGui::EndTable();
+
+            for (const auto &render_entity : entity->render_entities())
+            {
+                const auto material_index = render_entity.material_index();
+                const auto &material = scene.material_manager().material(material_index);
+
+                const auto *albedo_texture = scene.texture_manager().texture(material.albedo_texture_index);
+                if (albedo_texture)
+                {
+                    ::ImGui::Image(
+                        albedo_texture->native_handle(),
+                        ::ImVec2(64.0f, 64.0f),
+                        ::ImVec2(0.0f, 1.0f),
+                        ::ImVec2(1.0f, 0.0f));
+                }
+
+                const auto *normal_texture = scene.texture_manager().texture(material.normal_texture_index);
+                if (normal_texture)
+                {
+                    ::ImGui::SameLine();
+                    ::ImGui::Image(
+                        normal_texture->native_handle(),
+                        ::ImVec2(64.0f, 64.0f),
+                        ::ImVec2(0.0f, 1.0f),
+                        ::ImVec2(1.0f, 0.0f));
+                }
+
+                const auto *specular_texture = scene.texture_manager().texture(material.specular_texture_index);
+                if (specular_texture)
+                {
+                    ::ImGui::SameLine();
+                    ::ImGui::Image(
+                        specular_texture->native_handle(),
+                        ::ImVec2(64.0f, 64.0f),
+                        ::ImVec2(0.0f, 1.0f),
+                        ::ImVec2(1.0f, 0.0f));
+                }
+            }
 
             const auto &camera_data = scene.camera().data();
 
