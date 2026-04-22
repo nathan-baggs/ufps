e01136a9b0509cf71b9fd4157a873dfb9b73c130
Nathan Baggs <hello@nathanbaggs.com>
2026-04-22 22:10:52 +0100

    Await on timer

--- src/concurrency/awaitable_manager.h
+++ src/concurrency/awaitable_manager.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <chrono>
 #include <coroutine>
+#include <queue>
 
 #include "concurrency/concurrent_queue.h"
 #include "concurrency/thread_pool.h"
@@ -55,6 +57,34 @@ class AwaitableManager
         return Awaitable{*this};
     }
 
+    auto operator()(std::chrono::nanoseconds wait_time)
+    {
+        struct Awaitable
+        {
+            auto await_ready()
+            {
+                return false;
+            }
+
+            auto await_suspend(std::coroutine_handle<> h)
+            {
+                self.timer_queue_.push({
+                    .time_point = std::chrono::steady_clock::now() + wait_time,
+                    .handle = h,
+                });
+            }
+
+            auto await_resume()
+            {
+            }
+
+            AwaitableManager &self;
+            std::chrono::nanoseconds wait_time;
+        };
+
+        return Awaitable{*this, wait_time};
+    }
+
     auto pump() -> void
     {
         auto to_process = next_tick_queue_.yield();
@@ -66,11 +96,39 @@ class AwaitableManager
 
             pool_.add([handle = std::move(handle)] { handle.resume(); });
         }
+
+        while (!timer_queue_.empty())
+        {
+            auto timer_awaitable = timer_queue_.front();
+            if (timer_awaitable.time_point <= std::chrono::steady_clock::now())
+            {
+                pool_.add([handle = std::move(timer_awaitable.handle)] { handle.resume(); });
+            }
+            else
+            {
+                timer_queue_.push(std::move(timer_awaitable));
+                break;
+            }
+        }
     }
 
   private:
+    struct TimerAwaitable
+    {
+        std::chrono::steady_clock::time_point time_point;
+        std::coroutine_handle<> handle;
+
+        auto operator>(const TimerAwaitable &other) const -> bool
+        {
+            return time_point > other.time_point;
+        }
+    };
+    using TimePriorityQueue =
+        std::priority_queue<TimerAwaitable, std::vector<TimerAwaitable>, std::greater<TimerAwaitable>>;
+
     ThreadPool &pool_;
     ConcurrentQueue<std::coroutine_handle<>> next_tick_queue_;
+    ConcurrentQueue<TimerAwaitable, TimePriorityQueue> timer_queue_;
 };
 
 }
