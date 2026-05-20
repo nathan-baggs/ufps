509042bad39e87b87942dc66da4668ad45ad6292
Nathan Baggs <hello@nathanbaggs.com>
2026-05-20 21:22:51 +0100

    Add aligned new/delete

--- src/memory/new.cpp
+++ src/memory/new.cpp
@@ -1,7 +1,10 @@
 #include <cstddef>
+#include <memory>
+#include <new>
 #include <stdexcept>
 
 #include <windows.h>
+#include <winnt.h>
 
 namespace
 {
@@ -39,6 +42,37 @@ auto operator new[](std::size_t count) -> void *
     return ::operator new(count);
 }
 
+auto operator new(std::size_t count, std::align_val_t al) -> void *
+{
+    const auto alignment = static_cast<std::size_t>(al);
+    if (alignment <= MEMORY_ALLOCATION_ALIGNMENT)
+    {
+        return ::operator new(count);
+    }
+
+    const auto new_size = count + alignment + sizeof(void *);
+
+    auto *ptr = ::operator new(new_size);
+    auto size = new_size;
+
+    auto *offset_ptr = reinterpret_cast<void *>(reinterpret_cast<char *>(ptr) + alignment);
+
+    auto *aligned_ptr = std::align(alignment, count, offset_ptr, size);
+    if (aligned_ptr == nullptr)
+    {
+        throw std::bad_alloc{};
+    }
+
+    *(reinterpret_cast<void **>(aligned_ptr) - 1) = ptr;
+
+    return aligned_ptr;
+}
+
+auto operator new[](std::size_t count, std::align_val_t al) -> void *
+{
+    return ::operator new(count, al);
+}
+
 auto operator delete(void *ptr) noexcept -> void
 {
     ::HeapFree(heap(), 0, ptr);
@@ -46,7 +80,7 @@ auto operator delete(void *ptr) noexcept -> void
 
 auto operator delete[](void *ptr) noexcept -> void
 {
-    ::HeapFree(heap(), 0, ptr);
+    ::operator delete(ptr);
 }
 
 auto operator delete(void *ptr, std::size_t) noexcept -> void
@@ -57,6 +91,34 @@ auto operator delete(void *ptr, std::size_t) noexcept -> void
 auto operator delete[](void *ptr, std::size_t) noexcept -> void
 {
     ::operator delete(ptr);
+}
+
+auto operator delete(void *ptr, std::align_val_t al) noexcept -> void
+{
+    const auto alignment = static_cast<std::size_t>(al);
+    if (alignment <= MEMORY_ALLOCATION_ALIGNMENT)
+    {
+        ::operator delete(ptr);
+        return;
+    }
+
+    auto *orig_ptr = *(reinterpret_cast<void **>(ptr) - 1);
+    ::operator delete(orig_ptr);
+}
+
+auto operator delete(void *ptr, std::size_t, std::align_val_t al) noexcept -> void
+{
+    ::operator delete(ptr, al);
+}
+
+auto operator delete[](void *ptr, std::align_val_t al) noexcept -> void
+{
+    ::operator delete(ptr, al);
+}
+
+auto operator delete[](void *ptr, std::size_t, std::align_val_t al) noexcept -> void
+{
+    ::operator delete(ptr, al);
 }
 
 namespace ufps
--- tests/new_tests.cpp
+++ tests/new_tests.cpp
@@ -1,3 +1,4 @@
+#include <new>
 #include <windows.h>
 
 #include <gtest/gtest.h>
@@ -21,4 +22,38 @@ TEST(memory, new_array)
     ASSERT_GE(ufps::allocation_size(x), 100);
 
     ::operator delete[](x);
+}
+
+TEST(memory, new_aligned_win32_default_alignment)
+{
+    auto *x = ::operator new(4, std::align_val_t{4});
+    ASSERT_GE(ufps::allocation_size(x), 4);
+
+    ::operator delete(x, std::align_val_t{4});
+}
+
+TEST(memory, new_aligned_win32_larger_alignment)
+{
+    auto *x = ::operator new(4, std::align_val_t{32});
+    auto *orig_ptr = *(reinterpret_cast<void **>(x) - 1);
+    ASSERT_GE(ufps::allocation_size(orig_ptr), 4 + 32 + sizeof(void *));
+
+    ::operator delete(x, std::align_val_t{32});
+}
+
+TEST(memory, new_array_aligned_win32_default_alignment)
+{
+    auto *x = ::operator new[](4, std::align_val_t{4});
+    ASSERT_GE(ufps::allocation_size(x), 4);
+
+    ::operator delete(x, std::align_val_t{4});
+}
+
+TEST(memory, new_array_aligned_win32_larger_alignment)
+{
+    auto *x = ::operator new[](4, std::align_val_t{32});
+    auto *orig_ptr = *(reinterpret_cast<void **>(x) - 1);
+    ASSERT_GE(ufps::allocation_size(orig_ptr), 4 + 32 + sizeof(void *));
+
+    ::operator delete(x, std::align_val_t{32});
 }
