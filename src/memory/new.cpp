5d9d3d229606748bbde601521d8d8e8e29abd546
Nathan Baggs <hello@nathanbaggs.com>
2026-05-20 20:28:07 +0100

    Initial new/delete overload

---
+++ src/memory/new.cpp
@@ -0,0 +1,45 @@
+#include <cstddef>
+#include <stdexcept>
+
+#include <windows.h>
+
+namespace
+{
+
+auto heap() -> ::HANDLE
+{
+    static auto h = []
+    {
+        const auto h = ::HeapCreate(0, 0, 0);
+        if (h == INVALID_HANDLE_VALUE)
+        {
+            throw std::bad_alloc{};
+        }
+        return h;
+    }();
+
+    return h;
+}
+
+}
+
+auto operator new(std::size_t count) -> void *
+{
+    auto *ptr = ::HeapAlloc(heap(), 0, count);
+    if (ptr == nullptr)
+    {
+        throw std::bad_alloc{};
+    }
+
+    return ptr;
+}
+
+auto operator delete(void *ptr) noexcept -> void
+{
+    ::HeapFree(heap(), 0, ptr);
+}
+
+auto operator delete(void *ptr, std::size_t) noexcept -> void
+{
+    ::operator delete(ptr);
+}
