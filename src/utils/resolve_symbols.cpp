fe5c974cbe985b16eb00f6bc5aafb7139cd7231e
Nathan Baggs <hello@nathanbaggs.com>
2026-05-31 22:07:31 +0100

    Use libbacktrace for symbol resolution

--- src/utils/resolve_symbols.cpp
+++ src/utils/resolve_symbols.cpp
@@ -7,52 +7,68 @@
 #include <string>
 #include <vector>
 
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
-#include <dbghelp.h>
+#include <backtrace-supported.h>
+#include <backtrace.h>
+#include <cxxabi.h>
 
 #include "concurrency/lock.h"
 #include "utils/auto_release.h"
 #include "utils/error.h"
+#include "utils/exception.h"
 
 namespace
 {
-
-static auto g_sym_handle = []
+auto error_callback(void *, const char *msg, int errnum) -> void
 {
-    const auto current_process_handle = ::GetCurrentProcess();
-    auto h = ::HANDLE{};
+    throw ufps::Exception("backtrace error: {} {}", msg, errnum);
+}
 
-    ufps::ensure(
-        ::DuplicateHandle(
-            current_process_handle,
-            current_process_handle,
-            current_process_handle,
-            &h,
-            0,
-            FALSE,
-            DUPLICATE_SAME_ACCESS) != 0,
-        "failed to duplicate handle: {}",
-        ::GetLastError());
-
-    auto auto_handle = ufps::AutoRelease<::HANDLE, nullptr>{
-        h,
-        [](auto h)
-        {
-            ::SymCleanup(h);
-            ::CloseHandle(h);
-        }};
-
-    ufps::ensure(
-        ::SymInitialize(h, nullptr, TRUE) == TRUE, "failed to initialise symbol handler: {}", ::GetLastError());
-
-    ::SymSetOptions(SYMOPT_DEFERRED_LOADS | SYMOPT_UNDNAME);
-
-    return auto_handle;
+auto *g_state = []
+{
+    auto *state = ::backtrace_create_state(nullptr, BACKTRACE_SUPPORTS_THREADS, error_callback, nullptr);
+    return state;
 }();
 
-static auto g_lock = ufps::Lock{};
+auto syminfo_callback(void *data, std::uintptr_t pc, const char *symname, std::uintptr_t, std::uintptr_t) -> void
+{
+    auto *symbol = reinterpret_cast<std::string *>(data);
+
+    if (symname)
+    {
+        *symbol = std::format("{:x} {} ??:0", pc, symname);
+    }
+    else
+    {
+        *symbol = std::format("{:x} ?? ??:0", pc);
+    }
+}
+
+auto full_callback(void *data, std::uintptr_t pc, const char *filename, int lineno, const char *function) -> int
+{
+    auto *symbol = reinterpret_cast<std::string *>(data);
+    if (function)
+    {
+        auto status = int{};
+        const auto real_name = ::abi::__cxa_demangle(function, nullptr, nullptr, &status);
+        if (real_name)
+        {
+            *symbol = std::format("{:x} {} {}:{}", pc, real_name, filename, lineno);
+            std::free(real_name);
+        }
+        else
+        {
+            *symbol = std::format("{:x} {} {}:{}", pc, function, filename, lineno);
+        }
+    }
+    else
+    {
+        ::backtrace_syminfo(g_state, pc, syminfo_callback, error_callback, data);
+    }
+
+    return 0;
+}
+
+auto g_lock = ufps::Lock{};
 
 }
 
@@ -65,26 +81,14 @@ auto resolve_symbols(std::span<void *> call_stack) -> std::vector<std::string>
 
     return call_stack |
            std::views::transform(
-               [](auto e) -> std::string
+               [](auto e)
                {
-                   static constexpr auto max_string_length = 512u;
-
-                   auto symbol_buffer = std::array<std::byte, sizeof(::SYMBOL_INFO) + max_string_length>{};
-                   auto *symbol = new (symbol_buffer.data())::SYMBOL_INFO{};
-
-                   symbol->SizeOfStruct = sizeof(::SYMBOL_INFO);
-                   symbol->MaxNameLen = max_string_length + 1u;
-
-                   auto displacement = ::DWORD64{};
-
-                   auto res = ::SymFromAddr(g_sym_handle, reinterpret_cast<::DWORD64>(e), &displacement, symbol);
-
-                   return res == TRUE ? std::string(
-                                            symbol_buffer.data() + sizeof(::SYMBOL_INFO) - 1u,
-                                            symbol_buffer.data() + sizeof(::SYMBOL_INFO) - 1u + symbol->NameLen)
-                                      : std::string{};
+                   auto symbol = std::string{};
+                   ::backtrace_pcinfo(
+                       g_state, reinterpret_cast<std::uintptr_t>(e), full_callback, error_callback, &symbol);
+                   return symbol;
                }) |
+           std::views::filter([](const auto &e) { return !e.contains("register_frame_ctor"); }) |
            std::ranges::to<std::vector>();
 }
-
 }
