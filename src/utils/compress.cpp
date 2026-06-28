9986ec9d50d20e911ab88c99e4c07cc6e21a55a3
Nathan Baggs <hello@nathanbaggs.com>
2026-06-28 23:09:47 +0100

    Change compress to return std::expected

--- src/utils/compress.cpp
+++ src/utils/compress.cpp
@@ -8,7 +8,7 @@
 namespace ufps
 {
 
-auto compress(DataBufferView data) -> DataBuffer
+auto compress(DataBufferView data) -> std::expected<DataBuffer, std::string>
 {
     const auto compressed_buffer_size = ::ZSTD_compressBound(data.size_bytes());
     auto compressed_buffer = DataBuffer(compressed_buffer_size);
@@ -18,7 +18,7 @@ auto compress(DataBufferView data) -> DataBuffer
 
     if (::ZSTD_isError(compressed_size) == 1)
     {
-        throw Exception("failed to compress data: {}", ::ZSTD_getErrorName(compressed_size));
+        return std::unexpected(std::format("{}", ::ZSTD_getErrorName(compressed_size)));
     }
 
     compressed_buffer.resize(compressed_size);
