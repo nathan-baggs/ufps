b26026e6e50b3de2111a7b8e63552f78f562bdb7
Nathan Baggs <hello@nathanbaggs.com>
2026-08-05 22:42:18 +0100

    "Improve" quaternion

--- src/maths/quaternion.h
+++ src/maths/quaternion.h
@@ -3,8 +3,15 @@
 #include <format>
 #include <string>
 
+#include "maths/vector3.h"
+
 namespace ufps
 {
+
+namespace impl
+{
+
+}
 
 class Quaternion
 {
@@ -22,6 +29,29 @@ class Quaternion
     {
     }
 
+    constexpr Quaternion(const Vector3 &axis, float angle)
+    {
+        const auto sin_half = std::sin(angle / 2.0f);
+        const auto cos_half = std::cos(angle / 2.0f);
+
+        x = axis.x * sin_half;
+        y = axis.y * sin_half;
+        z = axis.z * sin_half;
+        w = cos_half;
+
+        *this = normalise(*this);
+    }
+
+    constexpr Quaternion(float yaw, float pitch, float roll);
+
+    static constexpr auto normalise(const Quaternion &q) -> Quaternion
+    {
+        const auto det = std::sqrt(q.x * q.x + q.y * q.y + q.z * q.z + q.w * q.w);
+        contract_assert(det != 0.0f);
+
+        return {q.x / det, q.y / det, q.z / det, q.w / det};
+    }
+
     auto to_string() const -> std::string;
 
     float x;
@@ -29,6 +59,25 @@ class Quaternion
     float z;
     float w;
 };
+
+constexpr auto operator*(const Quaternion &a, const Quaternion &b) -> Quaternion
+{
+    // thanks Harrand!
+    return {
+        (b.w * a.x) + (b.z * a.y) + (b.y * -a.z) + (b.x * a.w),
+        (b.z * -a.x) + (b.w * a.y) + (b.x * a.z) + (b.y * a.w),
+        (b.y * a.x) + (b.x * -a.y) + (b.w * a.z) + (b.z * a.w),
+        (b.x * -a.x) + (b.y * -a.y) + (b.z * -a.z) + (b.w * a.w)};
+}
+
+constexpr Quaternion::Quaternion(float yaw, float pitch, float roll)
+{
+    const auto qx = Quaternion{{1.0f, 0.0f, 0.0f}, pitch};
+    const auto qy = Quaternion{{0.0f, 1.0f, 0.0f}, yaw};
+    const auto qz = Quaternion{{0.0f, 0.0f, 1.0f}, roll};
+
+    *this = normalise(qx * qy * qz);
+}
 
 inline auto Quaternion::to_string() const -> std::string
 {
