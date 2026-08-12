65a3acaa3b1ae90a5b08d2db52981da5c61c9ca4
Nathan Baggs <hello@nathanbaggs.com>
2026-08-12 22:07:05 +0100

    Serialise render entities in an Entity

--- src/core/entity.h
+++ src/core/entity.h
@@ -29,6 +29,7 @@ class Entity
         Transform transform;
         AABB aabb;
         std::vector<RigidBody::Description> rigid_bodies;
+        std::vector<std::string> render_entities;
     };
 
     constexpr Entity(std::string name, std::vector<RenderEntityHandle> render_entities, Transform transform);
@@ -107,22 +108,24 @@ constexpr auto Entity::aabb() const -> const AABB &
 
 constexpr auto Entity::description() const -> Entity::Description
 {
+    const auto &[rem, ps] = services<RenderEntityManager, PhysicsSystem>();
+
+    auto render_entities = render_entities_ | std::views::filter([&](auto e) { return !!rem[e]; }) |
+                           std::views::transform([&](auto e) { return std::string{rem[e]->group_name()}; }) |
+                           std::ranges::to<std::vector>();
+    std::ranges::sort(render_entities);
+    static_cast<void>(std::ranges::unique(render_entities));
+
     return {
         .name = name_,
         .emissive_strength = emissive_strength_,
         .transform = transform_,
         .aabb = aabb_,
-        .rigid_bodies = rigid_bodies_ |
-                        std::views::transform(
-                            [](auto e)
-                            {
-                                auto &physics = service<PhysicsSystem>();
-                                return physics.rigid_body(e);
-                            }) |
+        .rigid_bodies = rigid_bodies_ | std::views::transform([&](auto e) { return ps.rigid_body(e); }) |
                         std::views::filter([](const auto &e) { return !!e; }) |
                         std::views::transform([](const auto &e) { return e->description(); }) |
                         std::ranges::to<std::vector>(),
-    };
+        .render_entities = std::move(render_entities)};
 }
 
 constexpr auto Entity::emissive_strength() const -> float
@@ -150,5 +153,4 @@ constexpr auto Entity::set_parent_transform(const Transform &transform) -> void
 {
     update_transforms(local_transform_, transform);
 }
-
 }
