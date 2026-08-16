b6af986d4c749791cc44e5ef0e17a08b4c2970be
Nathan Baggs <hello@nathanbaggs.com>
2026-08-16 09:16:24 +0100

    Add green

--- src/graphics/colour.h
+++ src/graphics/colour.h
@@ -19,6 +19,7 @@ inline constexpr Colour azure{.r = 0.0f, .g = 0.5f, .b = 1.0f};
 inline constexpr Colour black{.r = 0.0f, .g = 0.0f, .b = 0.0f};
 inline constexpr Colour yellow{.r = 1.0f, .g = 1.0f, .b = 0.0f};
 inline constexpr Colour magenta{.r = 1.0f, .g = 0.0f, .b = 1.0f};
+inline constexpr Colour green{.r = 0.0f, .g = 1.0f, .b = 0.0f};
 }
 
 inline auto to_string(const Colour &obj) -> std::string
