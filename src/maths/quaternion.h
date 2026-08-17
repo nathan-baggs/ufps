be0e6a70810fedf7168abdc4dfed9d9635a0f6fd
Nathan Baggs <hello@nathanbaggs.com>
2026-08-17 22:16:18 +0100

    Fix pitch/yaw swap bug

--- src/maths/quaternion.h
+++ src/maths/quaternion.h
@@ -72,8 +72,8 @@ constexpr auto operator*(const Quaternion &a, const Quaternion &b) -> Quaternion
 
 constexpr Quaternion::Quaternion(float yaw, float pitch, float roll)
 {
-    const auto qx = Quaternion{{1.0f, 0.0f, 0.0f}, yaw};
-    const auto qy = Quaternion{{0.0f, 1.0f, 0.0f}, pitch};
+    const auto qx = Quaternion{{1.0f, 0.0f, 0.0f}, pitch};
+    const auto qy = Quaternion{{0.0f, 1.0f, 0.0f}, yaw};
     const auto qz = Quaternion{{0.0f, 0.0f, 1.0f}, roll};
 
     *this = normalise(qz * qy * qx);
